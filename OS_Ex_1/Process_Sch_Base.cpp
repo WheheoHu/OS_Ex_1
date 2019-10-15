1d0c0907679d4d915d3dc2c6390b980971c338c6
文昊 胡 <hwh19981124@hotmail.com>
2019-10-15 10:49:55 +0800

    add base

---
+++ OS_Ex_1/Process_Sch_Base.cpp
@@ -0,0 +1,12 @@
+#include "Process_Sch_Base.h"
+
+
+
+Process_Sch_Base::Process_Sch_Base()
+{
+}
+
+
+Process_Sch_Base::~Process_Sch_Base()
+{
+}
---
+++ OS_Ex_1/Process_Sch_Base.h
@@ -0,0 +1,8 @@
+#pragma once
+class Process_Sch_Base
+{
+public:
+	Process_Sch_Base();
+	virtual ~Process_Sch_Base();
+};
+
